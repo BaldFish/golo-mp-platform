83bf48096b1f61c90e01dae901d6bdc2bbb74460
lwc <wence.lv@cnlaunch.com>
2018-11-23 11:08:02 +0800

    图标压缩，修改名字等

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="main_wrap">
       <router-view class="main" v-if="isRouterAlive"></router-view>
     </div>
-    <div class="footer_wrap">
+    <div class="footer_wrap" v-if="display">
       <ul>
         <li v-for="(item,index) of tabsParam" :class="{'active': index===nowIndex,}" @click="tabChange(index)">
           <img :src="item.img" alt="">
@@ -26,6 +26,7 @@
 
 <script>
   import _ from 'lodash'
+  
   export default {
     name: 'App',
     components: {},
@@ -37,35 +38,54 @@
     data() {
       return {
         isRouterAlive: true,
-        tabsParam: [
-          {img: require("@/common/images/baogaochaxun_dj.png"), text: "报告查询"},
-          {img: "", text: "订单"},
-          {img: "", text: "个人中心"}
-        ],
-        nowIndex: 0,
       }
     },
     beforeMount() {
-      //this.tabsParam[0] = {img: require("@/common/images/baogaochaxun_dj.png"), text: "报告查询"}
+      let path=this.$route.path
+      if(_.includes(path, "reportQuery", 1)){
+        this.tabsParam = [
+          {img: require("@/common/images/baogaochaxun_dj.png"), text: "报告查询"},
+          {img: "", text: "订单"},
+          {img: "", text: "个人中心"}
+        ];
+        this.nowIndex= 0
+        this.display=true
+      }else if(_.includes(path, "order", 1)){
+        this.tabsParam = [
+          {img: "", text: "报告查询"},
+          {img: require("@/common/images/baogaochaxun_dj.png"), text: "订单"},
+          {img: "", text: "个人中心"}
+        ];
+        this.nowIndex= 1
+        this.display=true
+      }else if(_.includes(path, "personalCenter", 1)){
+        this.tabsParam = [
+          {img: "", text: "报告查询"},
+          {img: "", text: "订单"},
+          {img: require("@/common/images/baogaochaxun_dj.png"), text: "个人中心"}
+        ];
+        this.nowIndex= 2
+        this.display=true
+      }else{
+        this.display=false
+      }
     },
     mounted() {
+      //console.log(this.a)
     },
     beforeUpdate() {
     },
     computed: {
-    a:function () {
-      return window.location.href
-    }
+      a: function () {
+        return this.$route.path
+      }
     },
     watch: {
-      a:function(){
-        console.log(a)
-        if (_.includes(a, "reportQuery",1)) {
-          console.log(true)
+      a: function () {
+        console.log(this.a)
+        if (_.includes(this.a, "reportQuery", 1)) {
         }
       },
-      $route(to,from) {
-      }
     },
     methods: {
       reload() {
@@ -80,21 +100,21 @@
       tabChange(index) {
         this.nowIndex = index;
         if (index === 0) {
-          this.tabsParam=[
+          this.tabsParam = [
             {img: require("@/common/images/baogaochaxun_dj.png"), text: "报告查询"},
             {img: "", text: "订单"},
             {img: "", text: "个人中心"}
           ];
           this.$router.push('/reportQuery');
-        }else if(index === 1){
-          this.tabsParam=[
+        } else if (index === 1) {
+          this.tabsParam = [
             {img: "", text: "报告查询"},
             {img: require("@/common/images/baogaochaxun_dj.png"), text: "订单"},
             {img: "", text: "个人中心"}
           ];
           this.$router.push('/order');
-        }else if(index === 2){
-          this.tabsParam=[
+        } else if (index === 2) {
+          this.tabsParam = [
             {img: "", text: "报告查询"},
             {img: "", text: "订单"},
             {img: require("@/common/images/baogaochaxun_dj.png"), text: "个人中心"}
@@ -122,6 +142,7 @@
     .footer_wrap {
       box-sizing border-box
       border-top solid 1px #e5e5e5; /*no*/
+      border-bottom solid 1px #e5e5e5; /*no*/
       width 750px
       height 98px
       min-width 640px
