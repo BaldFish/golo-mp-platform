aa7e89e8cc6519612ee1c79d44ef6d8a15bb2c4e
zhangzhenchang <591781589@qq.com>
2018-11-29 17:10:24 +0800

    订单列表

--- src/components/order/kilometreOrder/kilometreOrder.vue
+++ src/components/order/kilometreOrder/kilometreOrder.vue
@@ -1,6 +1,50 @@
 <template>
   <div class="kilometreOrder">
-    kilometreOrder
+    <div class="car-nav">
+      <ul>
+        <li v-for="(item,index) of tabsParam" :class="{'active': index===nowIndex}" @click="tabChange(index)">
+          <p>{{item.text}}</p>
+        </li>
+      </ul>
+    </div>
+    <section class="car-condition-list">
+      <ul>
+        <li>
+          <div class="order-num">
+            <p>订单号：yz294485553</p>
+            <p>待支付</p>
+          </div>
+          <div style="clear: both"></div>
+          <div class="order-details">
+            <p><label>车价号码：</label>JVLDL11223344545555</p>
+            <div class="order-time">
+              <p><label>订单时间：</label>2018.10.03  15：34</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="order-num">
+            <p>订单号：yz294485553</p>
+            <p>查询成功</p>
+          </div>
+          <div style="clear: both"></div>
+          <div class="order-details">
+            <p><label>车价号码：</label>JVLDL11223344545555</p>
+            <div class="order-time">
+              <p><label>订单时间：</label>2018.10.03  15：34</p>
+              <router-link to="/#">
+                <span class="to-report">查看里程报告&nbsp;》</span>
+              </router-link>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+    <section class="none-order">
+      <img src="@/common/images/empty.png" alt="">
+      <p>暂无查里程订单</p>
+      <input type="button" value="新建订单">
+    </section>
   </div>
 </template>
 
@@ -9,7 +53,18 @@
     name: "kilometreOrder",
     components: {},
     data() {
-      return {}
+      return {
+        nowIndex: 0,
+        tabsParam:[
+          {text: "全部状态"},
+          {text: "待支付"},
+          {text: "查询中"},
+          {text: "查询成功"},
+          {text: "查询失败"}
+        ],
+
+
+      }
     },
     created() {
     },
@@ -17,11 +72,132 @@
     },
     watch: {},
     computed: {},
-    methods: {},
+    methods: {
+      tabChange(index){
+        this.nowIndex = index
+        /*if (index == 0){
+
+        } else if (){
+
+        } else if (){
+
+        } else if (){
+
+        } else if (){
+
+        }*/
+      },
+
+    },
   }
 </script>
 
 <style scoped lang="stylus">
-  .kilometreOrder {
+  .car-nav{
+    height: 46px;
+    width: 686px
+    margin 0 auto
+    margin-top 27px
+    li{
+      width: 110px;
+      height: 46px;
+      line-height 46px;
+      border-radius: 10px;
+      border: solid 1px #666666; /*no*/
+      font-size: 22px; /*px*/
+      color: #999999;
+      text-align center
+      float left
+      margin-right 31px
+      box-sizing border-box
+    }
+    li:last-child{
+      margin-right 0
+    }
+    .active{
+      background-color: #5226f3;
+      color #ffffff
+      border: none
+    }
   }
+  .car-condition-list{
+    li{
+      width: 688px;
+      height: 240px;
+      background-color: #ffffff;
+      box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.09);
+      border-radius: 30px;
+      margin: 0 auto
+      margin-top 32px
+      .order-num{
+        font-size: 26px; /*px*/
+        color: #333333;
+        border-bottom 1px solid #eeeeee; /*no*/
+        height: 45px
+        margin: 0 20px;
+        padding-top: 34px;
+        padding-right 6px
+        p:nth-child(1){
+          float left
+        }
+        p:nth-child(2){
+          float right
+          color: #5226f3;
+        }
+      }
+      .order-details{
+        font-size: 26px; /*px*/
+        color: #333333;
+        margin: 0 20px;
+        label{
+          color: #666666;
+        }
+        p{
+          margin-top 35px
+        }
+        .order-time{
+          p{
+            float left
+          }
+          a{
+            float right
+            margin-top 35px
+            font-size: 24px; /*px*/
+            color: #333333;
+          }
+        }
+      }
+    }
+  }
+  .none-order{
+    width: 360px;
+    height: 360px;
+    background-color: #ffffff;
+    box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.09);
+    border-radius: 30px;
+    margin:0 auto
+    margin-top 303px
+    text-align center
+    img{
+      width: 86px;
+      height: 82px;
+      margin-top 67px
+      margin-bottom 20px
+    }
+    p{
+      font-size: 28px; /*px*/
+      color: #666666;
+      margin-bottom 56px
+    }
+    input{
+      width: 220px;
+      height: 64px;
+      background-color: #5226f3;
+      border-radius: 30px;
+      font-size: 28px; /*px*/
+      color: #ffffff;
+      outline none
+    }
+  }
+
 </style>
--- src/components/order/valuationOrder/valuationOrder.vue
+++ src/components/order/valuationOrder/valuationOrder.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="valuationOrder">
-    valuationOrder
+    <section class="query-container">
+      <div class="query-time">
+        <p>查询时间：2016.8.13 09:23</p>
+      </div>
+      <div class="query-detail">
+        <div class="car-brand">
+          <img src="@/common/images/empty.png" alt="">
+          <div class="car-text">
+            <p>奔驰/R级</p>
+            <p>2017款3.0自动 R3204MATINIYGFVC商务型 珍藏版</p>
+          </div>
+        </div>
+        <p class="car-production">北京/2013年01月</p>
+        <div class="car-mileage">
+          <label>行驶里程</label>
+          <input type="text" placeholder="请输入当前行驶里程">
+          <span>万公里</span>
+        </div>
+      </div>
+      <input class="submit" type="button" value="免费估价">
+    </section>
+    <section class="none-order">
+      <img src="@/common/images/empty.png" alt="">
+      <p>暂无查估价订单</p>
+      <input type="button" value="免费估价">
+    </section>
   </div>
 </template>
 
@@ -22,6 +47,108 @@
 </script>
 
 <style scoped lang="stylus">
-  .valuationOrder {
+.query-container{
+  width: 628px;
+  height: 440px;
+  background-color: #ffffff;
+  box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.09);
+  border-radius: 30px;
+  margin:0 auto
+  margin-top 40px
+  padding: 34px 30px
+  .query-time{
+    font-size: 26px; /*px*/
+    color: #333333;
+    border-bottom 1px solid #eee; /*no*/
+    padding-bottom 20px
   }
+  .query-detail{
+    border-bottom 1px solid #eee; /*no*/
+    .car-brand{
+      margin-top 32px
+      img{
+        width: 55px;
+        height: 56px;
+        float left
+        margin-right 22px
+      }
+      .car-text{
+        height: 56px;
+        line-height normal
+      }
+      .car-text p:nth-child(1){
+        font-size: 26px; /*px*/
+        color: #333333;
+      }
+      .car-text p:nth-child(2){
+        font-size: 20px; /*px*/
+        color: #666666;
+      }
+    }
+    .car-production{
+      font-size: 26px; /*px*/
+      color: #666666;
+      margin: 32px 0 38px 0
+    }
+    .car-mileage{
+      margin-bottom 26px
+      label{
+        font-size: 26px; /*px*/
+        color: #333333;
+        margin-right 45px
+      }
+      input{
+        font-size: 20px; /*px*/
+        outline none
+      }
+      span{
+        font-size: 24px; /*px*/
+        color: #5226f3;
+        float right
+        margin-right 10px
+      }
+    }
+  }
+  .submit{
+    width: 630px;
+    height: 84px;
+    background-color: #5226f3;
+    border-radius: 36px;
+    font-size: 36px; /*px*/
+    color: #ffffff;
+    margin-top 48px
+    outline none
+  }
+}
+.none-order{
+  width: 360px;
+  height: 360px;
+  background-color: #ffffff;
+  box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.09);
+  border-radius: 30px;
+  margin:0 auto
+  margin-top 303px
+  text-align center
+  img{
+    width: 86px;
+    height: 82px;
+    margin-top 67px
+    margin-bottom 20px
+  }
+  p{
+    font-size: 28px; /*px*/
+    color: #666666;
+    margin-bottom 56px
+  }
+  input{
+    width: 220px;
+    height: 64px;
+    background-color: #5226f3;
+    border-radius: 30px;
+    font-size: 28px; /*px*/
+    color: #ffffff;
+    outline none
+  }
+}
+
 </style>
