93f894f3d6ac46a3ac5e2450d00fd9c09292899c
zhangzhenchang <591781589@qq.com>
2018-11-30 19:14:04 +0800

    我的

--- src/components/addCar/addCar.vue
+++ src/components/addCar/addCar.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="addCar">
-    addCar
+    <section class="sec-form">
+      <div class="camera-notice" :class="{'hidden': isHidden}">
+        <p>扫一扫自动识别车架号</p>
+        <i @click="closeNotice"></i>
+      </div>
+      <div style="clear: both"></div>
+      <div class="sec-container">
+        <div class="car-frame">
+          <div class="car-frame-input">
+            <div class="frame-input">
+              <label>车架号码</label>
+              <input type="text" placeholder="请输入车架号码" maxlength="17" v-model="carFrameNum">
+            </div>
+            <div class="camera-box">
+              <img src="@/common/images/paizhao.png" alt="">
+            </div>
+            <div style="clear: both"></div>
+          </div>
+          <div class="car-frame-notice">
+            <p class="fl">已输入&nbsp;<span>{{carFrameNum.length}}</span>&nbsp;位，还差&nbsp;<span>{{17 - carFrameNum.length}}</span>&nbsp;位</p>
+            <router-link to="/VINExample" class="fr">车架号从哪里查？</router-link>
+            <div style="clear: both"></div>
+          </div>
+        </div>
+        <div class="sec-form-box">
+          <ul>
+            <li class="license-li">
+              <label>车牌号码</label>
+              <div class="license">
+                <span>京</span>
+              </div>
+              <input type="text" placeholder="请输入车牌号码">
+            </li>
+          </ul>
+        </div>
+        <input class="submit" type="button" value="确定新增">
+      </div>
+    </section>
   </div>
 </template>
 
@@ -9,19 +46,175 @@
     name: "addCar",
     components: {},
     data() {
-      return {}
+      return {
+        isHidden:false,
+        carFrame:'',
+
+      }
     },
     created() {
     },
     mounted() {
+      window.clearTimeout(timeOut);
+      //拍照提示20秒消失
+      let that = this;
+      let timeOut = window.setTimeout(function () {
+        that.isHidden = true;
+      }, 1000 * 20);
     },
     watch: {},
-    computed: {},
-    methods: {},
+    computed: {
+      carFrameNum : {
+        get: function(){
+          return this.carFrame;
+        },
+        set: function(val){
+          this.carFrame = val.toUpperCase();
+        }
+      },
+    },
+    methods: {
+      closeNotice(){
+        this.isHidden = true;
+      },
+
+    },
   }
 </script>
 
 <style scoped lang="stylus">
-  .addCar {
+  .sec-form{
+    width: 688px;
+    height: 500px;
+    box-shadow: 0 0 18px 2px rgba(0, 0, 0, 0.09);
+    border-radius: 30px;
+    margin:0 auto;
+    margin-top 58px
+    .camera-notice{
+      font-size: 20px; /*px*/
+      color: #333333;
+      text-align center
+      width: 270px;
+      height: 65px;
+      line-height 52px
+      background url("../../common/images/one.png") no-repeat center
+      background-size 100% 100%
+      float right
+      position: relative;
+      bottom: 24px;
+      right: 16px;
+      p{
+        float left
+        margin-left 20px
+      }
+      i{
+        width: 17px;
+        height: 17px;
+        display inline-block
+        background url("../../common/images/guanbi.png") no-repeat center
+        background-size 100% 100%
+        float right
+        margin-top 18px
+        margin-right 14px
+      }
+    }
+    .hidden{
+      visibility hidden
+    }
+    .sec-container{
+      height: auto;
+      padding: 52px 30px 0 30px;
+      position relative
+      bottom: 60px
+      .car-frame{
+        .car-frame-input{
+          border-bottom 1px solid #e5e5e5; /*no*/
+          padding-bottom 28px
+          .frame-input{
+            float left
+            label{
+              font-size: 28px; /*px*/
+              color: #333333;
+              margin-right 64px
+            }
+            input{
+              font-size: 26px; /*px*/
+              color: #333333;
+              outline: none;
+              width: 310px;
+            }
+          }
+          .camera-box{
+            float right
+            border-left 1px solid #bfbfbf; /*no*/
+            margin-right 20px
+            img{
+              width: 46px;
+              height: 36px;
+              margin-left 32px
+            }
+          }
+        }
+        .car-frame-notice{
+          font-size: 20px; /*px*/
+          color: #999999;
+          margin-top 24px
+          span{
+            color: #5226f3;
+          }
+          a{
+            font-size: 20px; /*px*/
+            color: #999999;
+          }
+        }
+
+      }
+      .sec-form-box{
+        li{
+          margin-top: 56px;
+          padding-bottom 28px
+          border-bottom 1px solid #e5e5e5; /*no*/
+          label{
+            font-size: 28px; /*px*/
+            color: #333333;
+            margin-right 64px
+          }
+          input{
+            font-size: 26px; /*px*/
+            color: #333333;
+            outline: none;
+            width: 270px;
+          }
+        }
+        .license-li{
+          label{
+            float left
+            margin-right 24px
+          }
+          .license{
+            float left
+            width: 40px;
+            height: 40px;
+            background-color: #c7b9fb;
+            text-align center
+            line-height 40px
+            color #fff;
+            font-size: 24px; /*px*/
+            margin-right: 7px;
+          }
+        }
+      }
+      .submit{
+        width: 630px;
+        height: 84px;
+        background-color: #5226f3;
+        border-radius: 36px;
+        line-height 84px;
+        color #ffffff
+        font-size: 36px; /*px*/
+        outline none
+        margin: 70px 0 32px 0;
+      }
+    }
   }
 </style>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,6 +1,46 @@
 <template>
   <div class="login">
-    login
+    <section class="login-container">
+      <ul>
+        <li>
+          <input type="text" placeholder="请输入手机号">
+        </li>
+        <li>
+          <input type="text" placeholder="请输入验证码">
+          <img src="@/common/images/banner_gujia.png" alt="">
+        </li>
+        <li>
+          <input type="text" placeholder="请输入短信验证码">
+          <div class="get_code" @click="getCode" v-if="codeValue">获取验证码</div>
+          <div class="count_down" v-else>倒计时（{{second}}）</div>
+        </li>
+      </ul>
+      <input class="submit" type="button" value="免注册登录">
+      <p class="login-notice">登录后可查看更多信息~</p>
+    </section>
+
+
+    <section class="coupon-box">
+      <input type="text" class="coupon-code" placeholder="请输入优惠券编码">
+      <input type="button" class="exchange" value="兑换">
+    </section>
+    <section class="coupon-tips">
+      <ul>
+        <li>
+          <div class="dot">
+            <span></span>
+          </div>
+          <p>持续关注公众平台“轱辘二手车评估”，第一时间获知更多优惠活动。</p>
+        </li>
+        <li>
+          <div class="dot">
+            <span></span>
+          </div>
+          <p>想找到我们？添加客服微信<span>（ID：dongcidaci8102）</span>或拨打电话&nbsp;<span>15210826130</span>，“小轱辘”为您在线答疑。</p>
+        </li>
+      </ul>
+
+    </section>
   </div>
 </template>
 
@@ -9,7 +49,11 @@
     name: "login",
     components: {},
     data() {
-      return {}
+      return {
+        codeValue: true,
+        second: 60,// 发送验证码倒计时
+
+      }
     },
     created() {
     },
@@ -17,11 +61,150 @@
     },
     watch: {},
     computed: {},
-    methods: {},
+    methods: {
+      getCode(){
+        //倒计时
+        let me = this;
+        me.codeValue = false;
+        let interval = window.setInterval(function () {
+          if ((me.second--) <= 0) {
+            me.second = 60;
+            me.codeValue = true;
+            window.clearInterval(interval);
+          }
+        }, 1000);
+      }
+    },
   }
 </script>
 
 <style scoped lang="stylus">
-  .login {
+.login-container{
+  width: 670px;
+  height: 855px;
+  background url("../../common/images/login_bg.png") no-repeat center
+  background-size 100% 100%
+  margin:0 auto
+  margin-top 20px
+  ul{
+    width 516px
+    margin: 0 auto;
+    padding-top: 250px;
+    li{
+      border-bottom: solid 1px #bfbfbf; /*no*/
+      font-size: 24px; /*px*/
+      input{
+        margin-left 12px
+        margin-bottom 20px
+        margin-top 76px
+        outline none
+      }
+      img{
+        width: 143px;
+        height: 64px;
+        float right
+        margin-top 38px
+        margin-right 12px
+      }
+      div{
+        float right
+        width: 180px;
+        height: 50px;
+        line-height 50px
+        text-align center
+        border-radius: 20px;
+        border: solid 1px #5226f3; /*no*/
+        font-size: 22px; /*px*/
+        color: #5226f3;
+        margin-top 54px
+        margin-right 12px
+      }
+      .count_down{
+        background-color: #7d7d7d;
+        color: #ffffff;
+        border none
+      }
+    }
   }
+  .submit{
+    width: 300px;
+    height: 84px;
+    background-color: #5226f3;
+    border-radius: 40px;
+    outline none
+    font-size: 36px; /*px*/
+    color: #ffffff;
+    margin-top 48px
+    margin-left 185px
+  }
+  .login-notice{
+    margin: 0 auto
+    margin-top 20px
+    text-align center
+    font-size: 24px; /*px*/
+    color: #999999;
+  }
+}
+
+
+.coupon-box{
+  width: 688px;
+  height: 360px;
+  background-color: #ffffff;
+  box-shadow: 0 0 18px 2px rgba(0, 0, 0, 0.09);
+  border-radius: 30px;
+  margin: 0 auto
+  margin-top 58px
+  .coupon-code{
+    width: 530px;
+    height: 80px;
+    background-color: #ffffff;
+    border-radius: 4px;
+    border: solid 1px #999999; /*no*/
+    outline none
+    font-size: 22px; /*px*/
+    padding-left 8px
+    margin: 72px 0 68px 78px
+  }
+  .exchange{
+    width: 630px;
+    height: 84px;
+    background-color: #5226f3;
+    border-radius: 36px;
+    font-size: 36px; /*px*/
+    color: #ffffff;
+    outline none
+    margin-left 30px
+  }
+}
+.coupon-tips{
+  width: 640px
+  margin: 0 auto
+  margin-top 120px
+  font-size: 24px; /*px*/
+  color: #333333;
+  li{
+    line-height normal
+    margin-top 42px
+    .dot{
+      height: 90px
+      float left
+      position: relative
+      top: 18px
+      margin-right 22px
+      span{
+        width: 24px;
+        height: 24px;
+        background-color: #5226f3;
+        display inline-block
+        border-radius 50%
+      }
+    }
+    p{
+      span{
+        font-size: 26px; /*px*/
+      }
+    }
+  }
+}
 </style>
