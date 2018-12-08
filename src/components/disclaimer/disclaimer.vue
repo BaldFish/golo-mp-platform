19c579d16e5d07909ffeec8cf18fd20f881c1541
lwc <wence.lv@cnlaunch.com>
2018-12-08 11:58:43 +0800

    免责声明页及路由配置跳转

---
+++ src/components/disclaimer/disclaimer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="disclaimer">
+    disclaimer
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "disclaimer",
+    components: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    mounted() {
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+  }
+</script>
+
+<style scoped lang="stylus">
+  .disclaimer {
+  }
+</style>
