8c70ce66d78605577b8c5c8e797ea01ee06a72f3
lwc <wence.lv@cnlaunch.com>
2018-12-11 18:38:28 +0800

    支付成功回调提示

--- src/components/order/kilometreOrder/kilometreOrder.vue
+++ src/components/order/kilometreOrder/kilometreOrder.vue
@@ -117,7 +117,12 @@
               if(res.err_msg == "get_brand_wcpay_request:ok" ){
                 // 使用以上方式判断前端返回,微信团队郑重提示：
                 //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-                window.location.href="http://pinggu.goloiov.com/order/vehicleConditionOrder"
+                that.errorMessage="订单提交成功";
+                that.errorTip=true;
+                window.setTimeout(function () {
+                  that.errorTip=false;
+                  window.location.href="http://pinggu.goloiov.com/order/vehicleConditionOrder"
+                },3000);
                 //that.$router.push('/order/vehicleConditionOrder')
               }else{
                 that.errorMessage="支付失败";
