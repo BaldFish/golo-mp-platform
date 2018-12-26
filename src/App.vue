5a0b3fe03adc5a414cb536f7f082084c877ee4e0
lwc <wence.lv@cnlaunch.com>
2018-12-26 10:08:01 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -84,7 +84,9 @@
          */
         ;(/iphone|ipod|ipad/i.test(navigator.appVersion)) && document.addEventListener('blur', (e) => {
         // 这里加了个类型判断，因为a等元素也会触发blur事件
-        ['input', 'textarea'].includes(e.target.localName) && e.target.scrollIntoView()
+        setTimeout(function () {
+          ['input', 'textarea'].includes(e.target.localName) && e.target.scrollIntoView()
+        },100)
       }, true)
     },
     beforeUpdate() {
