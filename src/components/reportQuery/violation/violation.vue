918a602cf90c8bc12d162ae6e52ba18a2b70fdc8
lwc <wence.lv@cnlaunch.com>
2019-01-08 16:35:48 +0800

    修改括号

--- src/components/reportQuery/violation/violation.vue
+++ src/components/reportQuery/violation/violation.vue
@@ -100,7 +100,7 @@
         </div>
       </section>
     </section>
-    <p class="cooperation">商务合作联系电话：13550564629（同微信）</p>
+    <p class="cooperation">商务合作联系电话：13550564629(同微信)</p>
     <el-dialog top="35vh" :visible.sync="centerDialogVisible" center :show-close="false" custom-class="fadongji">
       <img src="@/common/images/fadongji.png" alt="">
     </el-dialog>
