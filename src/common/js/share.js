f86b81fa735f45f10153516618636949b09e9680
lwc <wence.lv@cnlaunch.com>
2019-01-24 17:48:24 +0800

    测试

--- src/common/js/share.js
+++ src/common/js/share.js
@@ -20,7 +20,7 @@ export default {
       if (res.data.code === "200") {
         alert(2)
         let data = res.data.data;
-        alert(data)
+        alert(JSON.stringify(data))
         alert(data.signature)
         wx.config({
           debug: data.debug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
