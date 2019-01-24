9a9149a4cdcaf127763f9045f91820fae93f06f3
lwc <wence.lv@cnlaunch.com>
2019-01-24 18:14:51 +0800

    测试

--- src/common/js/share.js
+++ src/common/js/share.js
@@ -16,12 +16,8 @@ export default {
       method: 'GET',
       url: `https://wallet-api-test.launchain.org/v1/weixin/auth/share?url=${url}`,
     }).then(res => {
-      alert(1)
       if (res.data.code === "200") {
-        alert(2)
         let data = res.data.data;
-        alert(JSON.stringify(data))
-        alert(data.signature)
         wx.config({
           debug: data.debug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: data.appId, // 必填，公众号的唯一标识
@@ -32,19 +28,17 @@ export default {
         });
         //自定义“分享给朋友”及“分享到QQ”按钮的分享内容（1.4.0）
         wx.ready(function () {
-          alert(3)
+          alert(_this,shareTitle,shareUrl,shareImg,shareDesc)
           wx.updateAppMessageShareData({
             title: shareTitle, // 分享标题
             link: shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: shareImg, // 分享图标
             success: function () {
               // 用户确认分享后执行的回调函数
-              console.log("确认")
               alert("确认")
             },
             cancel: function () {
               // 用户取消分享后执行的回调函数
-              console.log("取消")
               alert("取消")
             }
           });
@@ -62,7 +56,6 @@ export default {
             },
             cancel: function () {
               // 用户取消分享后执行的回调函数
-              //console.log("取消")
               alert("取消")
               
             }
