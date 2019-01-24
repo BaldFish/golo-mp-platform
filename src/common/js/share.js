ff10815445e600d6ba86cb301b63de85dd6a62cc
lwc <wence.lv@cnlaunch.com>
2019-01-24 15:02:56 +0800

    测试

--- src/common/js/share.js
+++ src/common/js/share.js
@@ -16,11 +16,11 @@ export default {
       method: 'GET',
       url: `https://wallet-api-test.launchain.org/v1/weixin/auth/share?url=${url}`,
     }).then(res => {
-      //console.log(res.data);
+      console.log(res.data);
       if (res.data.code === "200") {
         let data = res.data.data;
         wx.config({
-          debug: data.debug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: data.appId, // 必填，公众号的唯一标识
           timestamp: data.timestamp, // 必填，生成签名的时间戳
           nonceStr: data.nonceStr, // 必填，生成签名的随机串
