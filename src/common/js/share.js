c66e1a7cb80b230d0e7345330f9c53df8276ff4b
lwc <wence.lv@cnlaunch.com>
2019-01-25 14:53:26 +0800

    测试

--- src/common/js/share.js
+++ src/common/js/share.js
@@ -20,40 +20,31 @@ export default {
         });
         //自定义“分享给朋友”及“分享到QQ”按钮的分享内容（1.4.0）
         wx.ready(function () {
-          alert(shareTitle)
-          alert(shareDesc)
-          alert(shareUrl)
-          alert(shareImg)
           wx.updateAppMessageShareData({
             title: shareTitle, // 分享标题
             desc: shareDesc, // 分享描述
-            link: location.origin+shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: location.origin+shareImg, // 分享图标
+            link: shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl:shareImg, // 分享图标
             success: function () {
               // 用户确认分享后执行的回调函数
-              alert("确认")
             },
             cancel: function () {
               // 用户取消分享后执行的回调函数
-              alert("取消")
             }
           });
           //自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容（1.4.0）
-          /*wx.updateTimelineShareData({
+          wx.updateTimelineShareData({
             title: shareTitle, // 分享标题
             desc: shareDesc, // 分享描述
             link: location.origin+shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: location.origin+shareImg, // 分享图标
             success: function () {
               // 用户确认分享后执行的回调函数
-              alert("确认")
             },
             cancel: function () {
               // 用户取消分享后执行的回调函数
-              alert("取消")
-      
             }
-          });*/
+          });
         });
       }
     }).catch(err => {
