fb103ae2dc4f173390c2bfe929f106a43fa6dfb6
lwc <wence.lv@cnlaunch.com>
2019-01-25 18:05:47 +0800

    人人道正式环境分支

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #version=`git symbolic-ref --short -q HEAD`
 
-image=`basename $PWD`:renrendao_develop
+image=`basename $PWD`:renrendao_cn
 
 aliyunImage=registry.cn-hangzhou.aliyuncs.com/launchain/$image
 
