7c98b68f26fd1cf22c8972ca2486f51ecf8774c2
lwc <wence.lv@cnlaunch.com>
2019-01-25 11:37:15 +0800

    测试

--- src/common/js/share.js
+++ src/common/js/share.js
@@ -3,7 +3,7 @@ import wx from 'weixin-js-sdk'
 
 export default {
   wxShare: function (_this,shareTitle, shareUrl, shareImg, shareDesc) {
-    let url = encodeURIComponent(window.location.href);
+    let url = encodeURI(window.location.href);
     axios({
       method: 'GET',
       url: `https://wallet-api-test.launchain.org/v1/weixin/auth/share?url=${url}`,
@@ -11,18 +11,15 @@ export default {
       if (res.data.code === "200") {
         let data = res.data.data;
         wx.config({
-          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: data.appId, // 必填，公众号的唯一标识
           timestamp: data.timestamp, // 必填，生成签名的时间戳
           nonceStr: data.nonceStr, // 必填，生成签名的随机串
           signature: data.signature, // 必填，签名，见附录1
-          jsApiList: [
-            'updateAppMessageShareData',
-            'updateTimelineShareData'
-          ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+          jsApiList: data.jsApiList// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
         });
         //自定义“分享给朋友”及“分享到QQ”按钮的分享内容（1.4.0）
-        //wx.ready(function () {
+        wx.ready(function () {
           alert(shareTitle)
           alert(shareDesc)
           alert(shareUrl)
@@ -42,7 +39,7 @@ export default {
             }
           });
           //自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容（1.4.0）
-          wx.updateTimelineShareData({
+          /*wx.updateTimelineShareData({
             title: shareTitle, // 分享标题
             desc: shareDesc, // 分享描述
             link: location.origin+shareUrl, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
@@ -56,8 +53,8 @@ export default {
               alert("取消")
       
             }
-          });
-        //});
+          });*/
+        });
       }
     }).catch(err => {
       console.log(err)
